391571c073a8b10fe8acf48e2d44a166cd975510
Bielski, Hubert (EXT) <hubert.bielski@skanska.pl>
2015-11-06 12:12:30 +0100

    fixed jasmine test runner

--- src/albums/album.js
+++ src/albums/album.js
@@ -1,10 +1,36 @@
+let _id = Symbol();
+let _artist = Symbol();
+let _name = Symbol();
+let _genre = Symbol();
+let _price = Symbol();
+
 export class Album {
 	constructor(id, artist, name, genre, price) {
-		this.id = id;
-		this.artist = artist;
-		this.name = name || artist;
-		this.genre = genre || "Metal";
-		this.price = price || 0;
+		this[_id] = id;
+		this[_artist] = artist;
+		this[_name] = name || artist;
+		this[_genre] = genre || "Metal";
+		this[_price] = price || 0;
+	}
+
+	get id() {
+		return this[_id];
+	}
+
+	get artist() {
+		return this[_artist];
+	}
+
+	get name() {
+		return this[_name];
+	}
+
+	get genre() {
+		return this[_genre];
+	}
+
+	get price() {
+		return this[_price];
 	}
 
 	get title() {
--- src/albums/album.spec.js
+++ src/albums/album.spec.js
@@ -11,14 +11,30 @@ describe("album", () => {
 	const genre = "genre name";
 	const price = 10.9;
 
-	it("can be created via constructor", () => {
-		let album = new Album(id, artist, name, genre, price);
-
-		expect(album).toBeDefined();
+	let createAlbum = () => new Album(id, artist, name, genre, price);
+	let assertAlbum = (album) => {
 		expect(album.id).toBe(id);
 		expect(album.artist).toBe(artist);
 		expect(album.name).toBe(name);
 		expect(album.genre).toBe(genre);
 		expect(album.price).toBe(price);
-	})
+	};
+
+	it("can be created via constructor", () => {
+		let album = createAlbum();
+
+		expect(album).toBeDefined();
+		assertAlbum(album);
+	});
+
+	it("is immutable", () => {
+		let album = createAlbum();
+		album.id = 145;
+		album.artist = "new artist";
+		album.name = "new name";
+		album.genre = "new genre";
+		album.price = 145;
+
+		assertAlbum(album);
+	});
 })
